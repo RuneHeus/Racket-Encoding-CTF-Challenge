8a4b81d59f56eac0152f325decaea5e7fb614eb6
RuneHeus <83946705+RuneHeus@users.noreply.github.com>
2026-01-20 15:00:51 +0100

    added password protector zip and encoding algorithm

--- Encoding.rkt
+++ Encoding.rkt
@@ -1,27 +1,21 @@
 #lang racket
 
-;;Performs a Caesar-style rotation on a single lowercase alphabetic character.
-;;Example:
-;;(rotate-char #\a 3) => #\d
-;;(rotate-char #\z 1) => #\a
-(define (rotate-char c n)
-  (define a (char->integer #\a))
-  (integer->char
-   (+ a (modulo (+ (- (char->integer c) a) n) 26))))
+;; Zet input om naar characters
+(define (to-char x)
+  (cond
+    [(integer? x) (integer->char (+ 48 x))] ; 7 -> #\7
+    [(symbol? x)  (string-ref (symbol->string x) 0)]
+    [else x]))
 
-;;Determines how much to rotate each character based on its position (index) in the string.
-;;The cycle repeats every 5 characters
+;; Roteer een character via ASCII
+(define (rotate-char ch n)
+  (integer->char (+ (char->integer ch) n)))
+
+;; Shift: 3 4 5 6 7
 (define (shift-for-index i)
-  (+ 3 (modulo i 5))) ; cycle: 3 4 5 6 7
+  (+ 3 (modulo i 5)))
 
-;;Reorders a list of blocks (each block is a list of characters)
-;;This is NOT a shuffle
-;;It is deterministic and reversible.
-;; Reordering rule:
-;;   1. Take all EVEN-indexed blocks (0, 2, 4, ...)
-;;   2. Then take all ODD-indexed blocks (1, 3, 5, ...)
-;;   3. Concatenate them in that order
-;;Example: blocks = (B0 B1 B2 B3) -> result = (B0 B2 B1 B3)
+;; Blokken herordenen: even eerst, dan oneven
 (define (reorder-blocks blocks)
   (append
    (for/list ([i (in-range (length blocks))] #:when (even? i))
@@ -29,31 +23,22 @@
    (for/list ([i (in-range (length blocks))] #:when (odd? i))
      (list-ref blocks i))))
 
-(define (encoding str)
-  ;;Creates a list of characters from given string
-  ;;Example: "Hello World!" -> '(H e l l o W o r l d !) or (cons #\H (cons #\e (cons #\l (cons #\l (cons #\o (cons #\W (cons #\o (cons #\r (cons #\l (cons #\d (cons #\! '())))))))))))
-  (define chars (string->list str))
+;; Split lijst in blokken van 4
+(define (chunk4 lst)
+  (if (null? lst)
+      '()
+      (cons (take lst (min 4 (length lst)))
+            (chunk4 (drop lst (min 4 (length lst)))))))
 
-  ;;This loop walks through the list of characters and applies a Caesar-style rotation to each one.
+;; Encoding
+(define (encoding xs)
+  (define chars (map to-char xs))
+
   (define rotated
-    (for/list ([c chars] [i (in-naturals)])
-      (rotate-char c (shift-for-index i))))
+    (for/list ([ch chars] [i (in-naturals)])
+      (rotate-char ch (shift-for-index i))))
 
-  ;; This uses a named `let` to implement a recursive loop that repeatedly takes the next 4 characters from the list.
-  (define blocks
-    (let loop ([lst rotated] [acc '()])
-      (if (null? lst)
-          (reverse acc)
-          (let-values ([(block rest)
-                        (split-at lst (min 4 (length lst)))])
-            (loop rest (cons block acc))))))
+  (apply append (reorder-blocks (chunk4 rotated))))
 
-  ;;Reorder the blocks and flatten them back into a single list
-  (define reordered
-    (apply append (reorder-blocks blocks)))
-  
-  ;;Convert the final list of characters back into a string
-  (list->string reordered))
-
-;;Deze moet nog weg, mag NIET in productie
-(encoding "CTF{GoIng_TO_CaMp_Bl00d}")
+(define (encoding->string xs)
+  (list->string (encoding xs)))
